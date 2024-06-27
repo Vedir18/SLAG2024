c9a124f9777b70f18b245fa91eef3cf20fb6589a
dragondale <dragondalegame@gmail.com>
2024-06-27 10:48:02 +0200

    animator player

--- Assets/FOLDER LIS/Scripts/PlayerController.cs
+++ Assets/FOLDER LIS/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Vector2 input;
     private Rigidbody _rb;
+    private Animator _animator;
     [SerializeField] private float speed = 1;
 
     // Start is called before the first frame update
@@ -21,6 +22,14 @@ public class PlayerController : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
         input = new Vector2(x, y);
         input.Normalize();
+        if(input.magnitude > 0.01f)
+        {
+            _animator.SetBool("B_iswalking", true);
+        }
+        else
+        {
+            _animator.SetBool("B_iswalking", false);
+        }
     }
 
     private void FixedUpdate()
